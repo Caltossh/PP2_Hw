c81c807c3418f61f141531a0a72371e103364920
Esjan Saltanat <123363592+Caltossh@users.noreply.github.com>
2023-02-02 17:34:38 +0600

    Update exercises.py

--- HW_3/PYTHON_functions/exercises.py
+++ HW_3/PYTHON_functions/exercises.py
@@ -10,16 +10,22 @@ def my_function():
 #exercise 3
 def my_function(fname, lname):
   print(fname)
+fname = "Salta"
+lname = "Dina"
+my_function(fname,lname)
 
 #exercise 4
 def my_function(x):
     return x + 5
+x = 4
+my_function(x)
 
 #exercise 5
 def my_function(*kids):
   print("The youngest child is " + kids[2])
+kids = ("Dina", "Salta", "SSSS")
 
 #exercise 6
 def my_function(**kid):
   print("His last name is " + kid["lname"])
-
+my_function(fname = "Dina", lname = "Salta")
